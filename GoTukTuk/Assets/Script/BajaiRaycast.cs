4c814af20e29ecfc6e4aa382f6545dcb24f3e76f
Candra Kharista Putra <candra.kharista@gmail.com>
2017-02-05 13:02:46 +0700

    script add and recode
    recode controller
    add streetprop

--- GoTukTuk/Assets/Script/BajaiRaycast.cs
+++ GoTukTuk/Assets/Script/BajaiRaycast.cs
@@ -6,6 +6,7 @@ public class BajaiRaycast : MonoBehaviour {
 
 	public float maxRayDistance = 25;
 	Vector3[] direction = {Vector3.forward, Vector3.down};
+	private Vector3 v;
 
 	// Use this for initialization
 	void Start () {
@@ -14,18 +15,29 @@ public class BajaiRaycast : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		Vector3 v = transform.position;
-		v.y -= 2.4f;
+		v = transform.position;
+		v.z += 4;
+
 		direction [0] = transform.TransformDirection (Vector3.forward);
 
 		Debug.DrawLine (v, v + direction[0] *maxRayDistance, Color.green);
+		Debug.DrawLine (v, v + direction[1] *maxRayDistance, Color.red);
 
-		Ray ray = new Ray (v, direction[0]);
-		RaycastHit hit;
-		if (Physics.Raycast(ray, out hit, maxRayDistance))
+		Ray ray1 = new Ray (v, direction[0]);
+		RaycastHit hit1;
+		if (Physics.Raycast(ray1, out hit1, maxRayDistance))
 		{
-			if (hit.collider.gameObject.name.Contains("jalan")){
-				Debug.Log("HIT - " + hit.collider.gameObject.name );
+			if (hit1.collider.gameObject.name.Contains("jalan")){
+				Debug.Log("HIT1 - " + hit1.collider.gameObject.name );
+			}
+		}
+
+		Ray ray2 = new Ray (v, direction[1]);
+		RaycastHit hit2;
+		if (Physics.Raycast(ray2, out hit2, maxRayDistance))
+		{
+			if (hit2.collider.gameObject.name.Contains("jalan")){
+				Debug.Log("HIT2 - " + hit2.collider.gameObject.name );
 			}
 		}
 	}
--- GoTukTuk/Assets/Script/BajajController.cs
+++ GoTukTuk/Assets/Script/BajajController.cs
@@ -14,8 +14,7 @@ public class BajajController : MonoBehaviour {
 	private Rigidbody rbBajai;
 	private Animator aBajai;
 	private bool state;
-	private int curAngle, targetAngle;
-	private string direction = "right";
+	private int curAngle;
 	private enum to {right, left};
 
 	// Use this for initialization
@@ -26,7 +25,7 @@ public class BajajController : MonoBehaviour {
 		playerDirection.setCurrentDirectionIndex (3);
 		playerDirection.setSpeed (3);
 
-		curAngle = 0; targetAngle = 0;
+		curAngle = 0;
 		Vector3 v = rbBajai.centerOfMass;
 		v.y = -0.9f;
 		rbBajai.centerOfMass = v;
@@ -34,23 +33,14 @@ public class BajajController : MonoBehaviour {
 
 	void FixedUpdate(){
 
-		//float speed = Input.GetAxis ("Vertical") > 0.2f ? 0.2f : Input.GetAxis ("Vertical") < -0.1f ? -0.1f : Input.GetAxis("Vertical");
 		float speed = 0.2f;
 		TireBL.motorTorque = maxTorque * speed;
 		TireBR.motorTorque = maxTorque * speed;
 
 		if (Input.GetKeyUp ("right")) {
-		//	aBajai.SetBool ("inputR", true);
-		//	direction = "right";
-		//	transform.rotation = Quaternion.Euler(0,curAngle,0);
-		//	targetAngle = curAngle + 90;
 			turnTo(to.right);
 		}
 		if (Input.GetKeyUp ("left")) {
-		//	aBajai.SetBool ("inputL", true);
-		//	direction = "left";
-		//	transform.rotation = Quaternion.Euler(0,curAngle,0);
-		//	targetAngle = curAngle - 90;
 			turnTo(to.left);
 		}
 
@@ -60,34 +50,7 @@ public class BajajController : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-	//	checkTurn (5, direction);
 		checkTurn();
-	}
-
-	int roundTo90(int num){
-		int res;
-		int cNum = num / 90;
-		int diff = num % 90;
-		if (diff >= 45) {
-			cNum += 1;
-		}
-		res = cNum * 90;
-		return res;
-	}
-
-	void checkTurn (int inc, string direction = "right"){
-		if (aBajai.GetBool ("inputR") || aBajai.GetBool ("inputL")) {
-			curAngle += direction == "right" ? inc : -inc;
-			transform.RotateAround( direction == "right" ? TireBR.transform.position : TireBL.transform.position, Vector3.up, direction == "right" ? inc : -inc);
-
-			if(curAngle == targetAngle){
-				curAngle = roundTo90 (targetAngle);
-				Debug.Log (curAngle);
-				transform.rotation = Quaternion.Euler(0,curAngle,0);
-				aBajai.SetBool ("inputR", false);
-				aBajai.SetBool ("inputL", false);
-			}
-		}
 	}
 
 	void turnTo(BajajController.to direction){
@@ -105,7 +68,6 @@ public class BajajController : MonoBehaviour {
 				curAngle += playerDirection.getSpeed ();
 				transform.RotateAround (TireBR.transform.position, Vector3.up, playerDirection.getSpeed ());
 				if (curAngle.Equals(playerDirection.getTargetDirection ())) {
-//					playerDirection.setCurrentDirectionIndex (playerDirection.getCurrentDirectionIndex() + 1);
 					curAngle = playerDirection.getTargetDirection () == 360? 0:playerDirection.getTargetDirection();
 					Debug.Log (curAngle);
 					transform.rotation = Quaternion.Euler (0, curAngle, 0);
@@ -116,7 +78,6 @@ public class BajajController : MonoBehaviour {
 				curAngle -= playerDirection.getSpeed ();
 				transform.RotateAround (TireBL.transform.position, Vector3.up, -(playerDirection.getSpeed ()));
 				if (curAngle.Equals(playerDirection.getTargetDirection ())) {
-					//playerDirection.setCurrentDirectionIndex (playerDirection.getCurrentDirectionIndex () - 1);
 					curAngle = playerDirection.getTargetDirection () == 360? 0:playerDirection.getTargetDirection();
 					Debug.Log (curAngle);
 					transform.rotation = Quaternion.Euler (0, curAngle, 0);
