9697086ef9d02087783b4c21caaedec12d0f6e90
erda <bagigdana161011@gmail.com>
2017-02-08 21:08:22 +0700

    cobak pop up pause

---
+++ GoTukTuk/Assets/Script/Menu/Pause.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour {
+
+	public GameObject PauseButton, PanelPause;
+
+	public void Start()
+	{
+		PanelPause.SetActive (false);
+		PauseButton.SetActive (true);
+	}
+
+	public void pause()
+	{
+		PanelPause.SetActive (true);
+		PauseButton.SetActive (false);
+		Time.timeScale = 0;
+	}
+
+	public void resume()
+	{
+		PanelPause.SetActive (false);
+		PauseButton.SetActive (true);
+		Time.timeScale = 1;
+	}
+}
