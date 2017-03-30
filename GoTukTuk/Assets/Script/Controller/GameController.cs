47271105e0ce49984df1922f55eadb9997dd303d
Candra Kharista Putra <candra.kharista@gmail.com>
2017-03-30 18:47:20 +0700

    Fix popup win

--- GoTukTuk/Assets/Script/Controller/GameController.cs
+++ GoTukTuk/Assets/Script/Controller/GameController.cs
@@ -24,6 +24,17 @@ public class GameController : MonoBehaviour {
 				isOnPopUp = true;
 				//switchCam ();
 				sceneScopeWin.GetComponent<FlipWebApps.BeautifulTransitions._Demo.Transitions.Scripts.TestController> ().TransitionIn();
+				Transform popUpWin = sceneScopeWin.GetComponent<FlipWebApps.BeautifulTransitions._Demo.Transitions.Scripts.TestController> ().TransitionFromButtons.transform.FindChild("Panel");
+				Transform sisaWaktu = popUpWin.Find ("Text").Find("sisaWaktu");
+				sisaWaktu.Find("value").GetComponent<TMPro.TextMeshProUGUI> ().text = GameController.gameModel.currentTimeMinutes + " : " + GameController.gameModel.currentTimeSecond;
+				Transform star = popUpWin.transform.FindChild ("starRes");
+				List<Transform> childs = new List<Transform> ();
+				foreach (Transform child in star) {
+					childs.Add(child);
+				}
+				for (int i = 0; i < GameController.gameModel.collectedStar; i++) {
+					childs[i].transform.FindChild ("value").gameObject.SetActive (true);
+				}
 			} else if (gameModel.IsGameOver && !isOnPopUp) {
 				isOnPopUp = true;
 				sceneScopeLose.GetComponent<FlipWebApps.BeautifulTransitions._Demo.Transitions.Scripts.TestController> ().TransitionIn();
