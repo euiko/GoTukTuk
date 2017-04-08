7df4ae7076085d88c5ccdf9401395a9e36ea1384
Candra Kharista Putra <candra.kharista@gmail.com>
2017-04-08 15:36:37 +0700

    Merge commit 'b2aaebb2dea656c32d76f293f44c6277bec34d42'
    # Conflicts:
    #	GoTukTuk/Assembly-CSharp.csproj
    #	GoTukTuk/Library/AssetImportState
    #	GoTukTuk/Library/CurrentLayout.dwlt
    #	GoTukTuk/Library/LastSceneManagerSetup.txt
    #	GoTukTuk/Library/ScriptAssemblies/Assembly-CSharp.dll.mdb
    #	GoTukTuk/Library/assetDatabase3

---
+++ GoTukTuk/Assets/Script/Controller/StageSelectController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StageSelectController : MonoBehaviour {
+
+	public GameObject StageJakarta, StageSurabaya, StageBandung, StageJogja;
+
+	// Use this for initialization
+	void Start () {
+		if (ES2.Exists ("game.dat?tag=stageone&encrypt=true&password=gotuktukbahagia")) {
+			StageJakarta.transform.FindChild("JakartaStage").GetComponent<Image> ().color = Color.white;
+			StageJakarta.transform.FindChild ("lock").gameObject.SetActive (false);
+		}
+	}
+	
+}
